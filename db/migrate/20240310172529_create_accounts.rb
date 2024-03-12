299c24a09d632faa0ea49464415a655012b61115
Vladislav Zhelezovsky <vladislav.zhelezovsky@gmail.com>
2024-03-13 00:17:03 +0300

    Add not null constraint to account name field

--- db/migrate/20240310172529_create_accounts.rb
+++ db/migrate/20240310172529_create_accounts.rb
@@ -1,7 +1,7 @@
 class CreateAccounts < ActiveRecord::Migration[7.1]
   def change
     create_table :accounts do |t|
-      t.string :name
+      t.string :name, null: false
       t.monetize :balance
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema[7.1].define(version: 2024_03_12_205110) do
   create_table "accounts", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.integer "balance_subunits", default: 0, null: false
     t.string "balance_currency", default: "BYN", null: false
     t.datetime "created_at", null: false
